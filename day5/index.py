995b7c32273487f762ddaaec11a49ea2b4751c68
Allan Montilla <allangmr7@gmail.com>
2022-06-16 09:37:40 -0500

    Project Day 5 Password Generator Console Python

---
+++ day5/index.py
@@ -0,0 +1,34 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+alias = []
+#select letters
+if nr_letters >=1:
+  for letter in range (1, nr_letters +  1):
+    letter_chose = random.choice(letters)
+    alias.append(letter_chose)
+#select numbers
+if nr_numbers >=1:
+  for number in range(1, nr_numbers +  1):
+    numbers_chose = random.choice(numbers)
+    alias.append(numbers_chose)
+#select symbols
+if nr_symbols >=1:
+  for symbol in range(1, nr_symbols +  1):
+    symbols_chose = random.choice(symbols)
+    alias.append(symbols_chose)
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+randomised = random.sample(alias, len(alias))
+password_random = ''.join(randomised)
+print(password_random)
---
+++ day5/readme.md
@@ -0,0 +1,9 @@
+# Example Password Generator - Python
+
+## What Is This?
+
+This is a simple Python application intended to provide a working example of Password Generator.
+
+## How To Use This
+
+1. Run `python index.py`
